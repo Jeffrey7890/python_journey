62f38d7ab85c98909d61b185e642263ff5bdd006
Jeffrey7890 <55333325+Jeffrey7890@users.noreply.github.com>
2022-06-01 10:04:14 +0100

    Create fourier.py

---
+++ Naive_fourier_series/fourier.py
@@ -0,0 +1,13 @@
+# 5/6/2021
+
+import math
+
+def fourier(time):
+	for i in range(0,5):
+		i=i*2+1
+		result = (4*math.sin(i*time))/(i*math.pi)
+		result2 = (4*math.cos(i*time))/(i*math.pi)
+
+		print(result,result2)
+
+fourier(0.05)
